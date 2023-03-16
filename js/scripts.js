11e249f3e3e38ef9f5ce85f07739fae9a44c6c2b
Emily <eathomas1@ualr.edu>
2023-03-16 01:12:25 -0400

    NPM Scripts Part 02

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
-src="node_modules/jquery/dist/jquery.slim.min.js"
-src="node_modules/popper.js/dist/umd/popper.min.js"
-src="node_modules/bootstrap/dist/js/bootstrap.min.js"
-src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-
 $(document).ready(function(){
     $("#mycarousel").carousel( { interval: 2000 } );
     $("#carousel-pause").click(function(){
