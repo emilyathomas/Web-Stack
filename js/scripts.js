2235dd58a58d381a7d395b898e5500061b4dc9c9
Emily <eathomas1@ualr.edu>
2023-03-17 18:25:57 -0400

    Assignment 4 & Unusuable Gulp

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,39 @@ $(document).ready(function(){
     $("#carousel-play").click(function(){
         $("#mycarousel").carousel('cycle');
     });
+
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fapause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fapause');
+            $("#carouselButton").children("span").addClass('faplay');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('faplay');
+            $("#carouselButton").children("span").addClass('fapause');
+        }
+    });
+       
+
+    $("#reserveBtn").click(function(){
+        $("#reserveModal").modal("show");
+    });
+    $("#reserveClose").click(function(){
+        $("#reserveModal").modal("hide");
+    });
+    $("#reserveClose2").click(function(){
+        $("#reserveModal").modal("hide");
+    });
+
+
+    $("#loginBtn").click(function(){
+        $("#loginModal").modal("show");
+    });
+    $("#loginClose").click(function(){
+        $("#loginModal").modal("hide");
+    });
+    $("#loginClose2").click(function(){
+        $("#loginModal").modal("hide");
+    });
 });
